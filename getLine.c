0c16b55993ab2c9a63dbe199c3ed7540706cbd4e
Daniel-Kav <99611930+Daniel-Kav@users.noreply.github.com>
2023-04-27 02:25:27 +0300

    Update getLine.c

--- getLine.c
+++ getLine.c
@@ -138,7 +138,7 @@ int _getline(info_t *info, char **ptr, size_t *length)
 	c = _strchr(buf + i, '\n');
 	k = c ? 1 + (unsigned int)(c - buf) : len;
 	new_p = _realloc(p, s, s ? s + k : k + 1);
-	if (!new_p) /* MALLOC FAILURE! */
+	if (!new_p) /* MALLOC FAILURE! */ 
 		return (p ? free(p), -1 : -1);
 
 	if (s)
