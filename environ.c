1bf511d71607a72a1eeda333b6259a6a94c0f0e1
daniel-kav <dankavatha01@gmail.com>
2023-07-26 11:46:42 +0300

    code refactor

--- environ.c
+++ environ.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include "lesh.h"
 
 /**
  * _myenv - prints the current environment
--- errors.c
+++ errors.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include "lesh.h"
 
 /**
  *_eputs - prints an input string
